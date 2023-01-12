d15231dbe69269b608224e9a943e2eddc84d5cd5
Stefan Warmerdam <stefan.warmerdam@alliander.com>
2023-01-12 09:18:38 +0100

    fix conflict

--- hodlv2/main.py
+++ hodlv2/main.py
@@ -30,7 +30,7 @@ class Worker:
         self.config = config
         self.notify = Notify(self.config)
 
-        self.version= f"HODLv2 v2023.1"
+        self.version= f"HODLv2 version:{__version__}"
         self.notify.send(self.version, "INFO", logging_only=True)
         print (self.version)
 
