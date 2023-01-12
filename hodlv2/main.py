62e92b39fddb45c2a9990f33e8c0b779d618a99e
Stefan Warmerdam <stefan.warmerdam@alliander.com>
2023-01-12 09:15:40 +0100

    version

--- hodlv2/main.py
+++ hodlv2/main.py
@@ -31,7 +31,7 @@ class Worker:
         self.config = config
         self.notify = Notify(self.config)
 
-        self.version = f"HODLv2 {__version__}"
+        self.version = f"HODLv2 version:{__version__}"
         self.notify.send(self.version, "INFO", logging_only=True)
         print (self.version)
 
