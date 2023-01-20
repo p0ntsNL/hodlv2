96167489f91fae8e5eebf907e4fe048df69d004a
Stefan Warmerdam <stefan.warmerdam@alliander.com>
2023-01-21 00:36:26 +0100

    back to localhost

--- hodlv2/web/configuration/__init__.py
+++ hodlv2/web/configuration/__init__.py
@@ -8,6 +8,6 @@ app.secret_key = "d07651a4be534b30c6b844705020f1c5"
 connection_params = {
     "user": "",
     "password": "",
-    "host": "192.168.178.54",
+    "host": "localhost",
     "port": "27017",
 }
